0c91e0a39a91faacd143559ca00f7437bc11440c
FinGuru Inc <workingaws1@gmail.com>
2022-12-21 16:58:32 -0500

    insignificant update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+// import Amplify, {API} from 'aws-amplify'
+import config from './aws-exports';
+import { Button, withAuthenticator } from '@aws-amplify/ui-react';
+import { useEffect, useState } from 'react';
+import { API } from 'aws-amplify';
 
-function App() {
+// Amplify.configure(config);
+
+const App = ({signOut}) => {
+
+  const [petName, setPetName] = useState('')
+  const [petType, setPetType] = useState('')
+  const [pets, setPets] = useState([])
+
+  useEffect (() => {
+    API.get('petsname', '/pets/name').then(
+      (res) => {setPets([ ...res])}
+    )
+    console.log(pets)
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    API.post('petsname', '/pets', {
+      body: {
+        name: petName,
+        type: petType,
+      }
+    }
+    ).then(() => {
+      setPets([...pets, {name: petName, type: petType}])
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      happy coding!
+    <form onSubmit={handleSubmit}>
+      <input value={petName} placeholder="fiddo" onChange={(e) => setPetName(e.target.value)}/>
+      <input value={petType} placeholder="fiddo" onChange={(e) => setPetType(e.target.value)}/>
+      <button> Submit! </button>
+    </form>
+    <ul>
+      {pets.map(pet => <li> {pet.name}</li>)}
+    </ul>
+    <Button onClick={signOut}>Sign Out</Button>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
